f968ad0c3deb94821a82217b38ad08101afc4e02
Ben Asher <benasher44@gmail.com>
2016-03-30 19:58:21 -0700

    initial commit

---
+++ ShapeSwift/ShapeSwift.h
@@ -0,0 +1,19 @@
+//
+//  ShapeSwift.h
+//  ShapeSwift
+//
+//  Created by Benjamin Asher on 3/30/16.
+//  Copyright © 2016 Benjamin Asher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ShapeSwift.
+FOUNDATION_EXPORT double ShapeSwiftVersionNumber;
+
+//! Project version string for ShapeSwift.
+FOUNDATION_EXPORT const unsigned char ShapeSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShapeSwift/PublicHeader.h>
+
+
---
+++ ShapeSwiftTests/ShapeSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  ShapeSwiftTests.swift
+//  ShapeSwiftTests
+//
+//  Created by Benjamin Asher on 3/30/16.
+//  Copyright © 2016 Benjamin Asher. All rights reserved.
+//
+
+import XCTest
+@testable import ShapeSwift
+
+class ShapeSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
