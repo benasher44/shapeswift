b336aadb014f42977c96fa496f32d73641ca5fb3
Ben Asher <benasher44@gmail.com>
2016-04-29 08:36:07 -0700

    Added Gemfile. Moved debug string extensions

---
+++ ShapeSwift/ShapeFileParserDebugging.swift
@@ -0,0 +1,38 @@
+//
+//  ShapeFileParserDebugging.swift
+//  ShapeSwift
+//
+//  Created by Ben Asher on 4/29/16.
+//  Copyright © 2016 Benjamin Asher. All rights reserved.
+//
+
+import Foundation
+
+extension CoordinateBounds: CustomDebugStringConvertible {
+  var debugDescription: String {
+    return "{\(min), \(max)}"
+  }
+}
+
+extension BoundingBox: CustomDebugStringConvertible {
+  var debugDescription: String {
+    return [
+      "X: \(x)",
+      "Y: \(y)",
+      "Z: \(z)",
+      "M: \(m)",
+      ].joinWithSeparator(" ")
+  }
+}
+
+extension ShapeFileHeader: CustomDebugStringConvertible {
+  var debugDescription: String {
+    return [
+      "File Code: \(fileCode)",
+      "File Length: \(fileLength)",
+      "Version: \(version)",
+      "Shape Type: \(shapeType)",
+      "Bounding Box: \(boundingBox.debugDescription)",
+      ].joinWithSeparator("\n")
+  }
+}
