295e27bcfe1289697106849f31a0ce95dd34be37
Ben Asher <benasher44@gmail.com>
2016-05-05 10:11:17 -0700

    multipatch

--- ShapeSwift/SHPFileRecord.swift
+++ ShapeSwift/SHPFileRecord.swift
@@ -8,12 +8,30 @@
 
 import Foundation
 
+private let noDataValue: Double = -pow(10, 38)
+
+func valueOrNilForOptionalValue(value: CoordinateBounds) -> CoordinateBounds? {
+  if value.min < noDataValue || value.max < noDataValue{
+    return nil
+  } else {
+    return value
+  }
+}
+
+func valueOrNilForOptionalValue(value: Coordinate) -> Coordinate? {
+  if value.x < noDataValue || value.x < noDataValue{
+    return nil
+  } else {
+    return value
+  }
+}
+
 protocol ShapeFileRecord {
-  init?(data: NSData, start: Int) throws
+  init?(data: NSData, range: Range<Int>) throws
 }
 
 extension ShapeFileRecord {
-  static func recordForShapeType(shapeType: ShapeType, data: NSData, start: Int) throws -> ShapeFileRecord? {
+  static func recordForShapeType(shapeType: ShapeType, data: NSData, range: Range<Int>) throws -> ShapeFileRecord? {
     var type: ShapeFileRecord.Type
     switch shapeType {
     case .point:
@@ -21,7 +39,7 @@ extension ShapeFileRecord {
     default:
       return nil
     }
-    return try type.init(data: data, start: start)
+    return try type.init(data: data, range: range)
   }
 }
 
@@ -38,8 +56,8 @@ struct ShapeFilePointRecordParser {
 struct ShapeFilePointRecord: ShapeFileRecord {
   let point: Coordinate
 
-  init?(data: NSData, start: Int) throws {
-    let parser = ShapeFilePointRecordParser(start: start)
+  init?(data: NSData, range: Range<Int>) throws {
+    let parser = ShapeFilePointRecordParser(start: range.startIndex)
     point = try parser.point.parse(data)!
   }
 }
@@ -61,8 +79,8 @@ struct ShapeFileMultiPointRecord: ShapeFileRecord {
   let box: BoundingBoxXY
   let points: [Coordinate]
 
-  init?(data: NSData, start: Int) throws {
-    let parser = try ShapeFileMultiPointRecordParser(data: data, start: start)!
+  init?(data: NSData, range: Range<Int>) throws {
+    let parser = try ShapeFileMultiPointRecordParser(data: data, start: range.startIndex)!
     box = try parser.box.parse(data)!
     points = try parser.points.parse(data)!
   }
@@ -72,14 +90,54 @@ struct ShapeFileMultiPointRecord: ShapeFileRecord {
 
 struct ShapeFileMultiPatchRecordParser {
   let box: ShapeDataParser<LittleEndian<BoundingBoxXY>>
-  let numParts: ShapeDataParser<LittleEndian<Int32>>
-  let numPoints: ShapeDataParser<LittleEndian<Int32>>
-  let parts: ShapeDataArrayParser<LittleEndian<MultiPatchPartType>>
+  let parts: ShapeDataArrayParser<LittleEndian<Int32>>
+  let partTypes: ShapeDataArrayParser<LittleEndian<MultiPatchPartType>>
+  let points: ShapeDataArrayParser<LittleEndian<Coordinate>>
+  let zBounds: ShapeDataParser<LittleEndian<CoordinateBounds>>
+  let zPoints: ShapeDataArrayParser<LittleEndian<Coordinate>>
+  let mBounds: ShapeDataParser<LittleEndian<CoordinateBounds>>
+  let mPoints: ShapeDataArrayParser<LittleEndian<Coordinate>>
+
+  init(data: NSData, start: Int) throws {
+    box = ShapeDataParser<LittleEndian<BoundingBoxXY>>(start: start)
+    let numPartsParser = ShapeDataParser<LittleEndian<Int32>>(start: box.end)
+    let numParts = try Int(numPartsParser.parse(data)!)
+    let numPointsParser = ShapeDataParser<LittleEndian<Int32>>(start: numPartsParser.end)
+    let numPoints = try Int(numPointsParser.parse(data)!)
+    parts = ShapeDataArrayParser<LittleEndian<Int32>>(start: numPointsParser.end, count: numParts)
+    partTypes = ShapeDataArrayParser<LittleEndian<MultiPatchPartType>>(start: parts.end, count: numParts)
+    points = ShapeDataArrayParser<LittleEndian<Coordinate>>(start: partTypes.end, count: numPoints)
+    zBounds = ShapeDataParser<LittleEndian<CoordinateBounds>>(start: points.end)
+    zPoints = ShapeDataArrayParser<LittleEndian<Coordinate>>(start: zBounds.end, count: numPoints)
+    mBounds = ShapeDataParser<LittleEndian<CoordinateBounds>>(start: zPoints.end)
+    mPoints = ShapeDataArrayParser<LittleEndian<Coordinate>>(start: mBounds.end, count: numPoints)
+  }
 }
 
 struct ShapeFileMultiPatchRecord: ShapeFileRecord {
-  init?(data: NSData, start: Int) throws {
-
+  let box: BoundingBoxXY
+  let parts: [Int]
+  let partTypes: [MultiPatchPartType]
+  let points: [Coordinate]
+  let zBounds: CoordinateBounds
+  let zPoints: [Coordinate]
+  let mBounds: CoordinateBounds?
+  let mPoints: [Coordinate]
+  init?(data: NSData, range: Range<Int>) throws {
+    let parser = try ShapeFileMultiPatchRecordParser(data: data, start: range.startIndex)
+    box = try parser.box.parse(data)!
+    parts = try parser.parts.parse(data)!.map(Int.init)
+    partTypes = try parser.partTypes.parse(data)!
+    points = try parser.points.parse(data)!
+    zBounds = try parser.zBounds.parse(data)!
+    zPoints = try parser.zPoints.parse(data)!
+    if range.endIndex > parser.mBounds.start {
+      mBounds = try parser.mBounds.parse(data).flatMap(valueOrNilForOptionalValue)
+      mPoints = try parser.mPoints.parse(data)!.flatMap(valueOrNilForOptionalValue)
+    } else {
+      mBounds = nil
+      mPoints = []
+    }
   }
 }
 
