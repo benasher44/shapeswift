d3e900b983925ec5521761e9eae4033bb953f995
Noah Gilmore <noah@yelp.com>
2016-09-02 09:55:55 -0700

    Add dbf fixtures, some crash but others do not

--- ShapeSwiftTests/DBFFile/DBFFileHeaderTest.swift
+++ ShapeSwiftTests/DBFFile/DBFFileHeaderTest.swift
@@ -11,9 +11,48 @@ import XCTest
 
 class DBFFileHeaderTest: XCTestCase {
   func testParser() {
-    let url = Bundle(for: type(of: self)).url(forResource: "sfsweeproutes", withExtension: "dbf")!
+    assertCanParse(filename: "sfsweeproutes")
+  }
+
+  func testCP1251() {
+    assertCanParse(filename: "cp1251")
+  }
+
+  func testDBASE03() {
+    assertCanParse(filename: "dbase_03")
+  }
+
+  func testDBASE30() {
+    assertCanParse(filename: "dbase_30")
+  }
+
+  func testDBASE31() {
+    assertCanParse(filename: "dbase_31")
+  }
+
+  func testDBASE83() {
+    assertCanParse(filename: "dbase_83")
+  }
+
+  func testDBASE83NoMemo() {
+    assertCanParse(filename: "dbase_83_missing_memo")
+  }
+
+  func testDBASE8B() {
+    assertCanParse(filename: "dbase_8b")
+  }
+
+  func testDBASEF5() {
+    assertCanParse(filename: "dbase_f5")
+  }
+
+  private func assertCanParse(filename: String) {
+    let url = Bundle(for: type(of: self)).url(forResource: filename, withExtension: "dbf")!
     let data = try! Data(contentsOf: url, options: .mappedIfSafe)
-    let header = try? DBFFileHeader(data: data, start: 0)
-    XCTAssertNotNil(header)
+    if let header = try? DBFFileHeader(data: data, start: 0) {
+      print(header)
+    } else {
+      XCTFail("Could not parse header \(filename).")
+    }
   }
 }
